df0e36b3dc830fcb20c00e0dc333f2e50413ed80
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-25 12:39:12 +0100

    node.cpp completed, still working on list.cpp

--- list.cpp
+++ list.cpp
@@ -4,13 +4,19 @@
 
 #include <memory>
 
-List::List() {
+List::List(int value = 0) {
+	head = NULL;
 }
 
 List::~List() {
+	
 }
 
 void List::push_front(const std::string &s) {
+	Node* newNode = new Node;
+	newNode->next = next;
+	next = newNode;
+	newNode->string = stoi(s);
 }
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
--- node.cpp
+++ node.cpp
@@ -5,18 +5,35 @@
 #include <iostream>
 
 bool node_number_compare(const Node *a, const Node *b) {
+	return a->number > b->number;
 }
 
 bool node_string_compare(const Node *a, const Node *b) {
+	int a_size = a->string.length();
+	int b_size = b->string.length();
+	
+	return a_size > b_size;
 }
 
 int void_number_compare(const void *a, const void *b) {
+	const int* a_num = (const int*) a;
+	const int* b_num = (const int*) b;
+	
+	return (*a_num > *b_num) ? 0 : 1;
 }
 
 int void_string_compare(const void *a, const void *b) {
+	const std::string* a_str = (const std::string*) a;
+	const std::string* b_str = (const std::string*) b;
+	
+	return (a_str->length() > b_str->length()) ? 1 : 0;
 }
 
 void dump_node(Node *n) {
+	while(n != nullptr){
+		std::cout << n->string << " " << n->number << std::endl;
+		n = n->next;
+	}
 }
 
 // vim: set sts=4 sw=4 ts=8 expandtab ft=cpp:
