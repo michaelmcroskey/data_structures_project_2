25eaaec3a9f68e84c652ff8f0cbc24814fcd2055
michaelmcroskey <michaelmcroskey@users.noreply.github.com>
2016-09-27 16:07:49 +0100

    Stl sorting working better (struct Node * array)

--- stl.cpp
+++ stl.cpp
@@ -6,36 +6,21 @@
 #include <iostream>
 
 void stl_sort(List &l, bool numeric) {
-
-	std::vector<int> v;
-	struct Node *p = l.head;
-	while(1){
-		v.push_back(p->number);
-		if (p->next == nullptr) break;
+	
+	int size = l.size + 1;
+	
+	struct Node *a[size];		// create array
+	struct Node *p = l.head;		// current pointer at head
+	a[0] = p;					// first element is head pointer
+	
+	int i = 1;
+	
+	while(p != nullptr){			// copy element-by-element
+		a[i++] = p;
 		p = p->next;
 	}
 	
-	std::sort(v.begin(), v.end());
-	
-//	int array[l.size + 1];
-//	struct Node *p = l.head;
-//	array[0] = p->number;
-//	
-//	int i = 1;
-//	
-//	while(1){
-//		array[i++] = p->number;
-//		if (p->next == nullptr) break;
-//		p = p->next;
-//	}
-//	
-//	std::sort(std::begin(array), std::end(array));
-	
-//The first sorting mode is to use the std::sort method in the stl.cpp file. Because our struct List does not implement random access iterators, we will need to copy the struct Nodes in the struct List into another container such as an array. Once we have this copy, we can then call the std::sort function on this copy with the appropriate comparison function. Finally, we must update the links between the struct Nodes to reflect the sorted order.
-//
-//	Hint: You should store struct Node * in your copy array.
-//
-//	Hint: Make sure you set the head of the struct List after you have set the links of all the struct Nodes.
+	std::sort(a+1, a+size, node_number_compare);
 	
 }
 
